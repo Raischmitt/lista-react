ee6a0a92cdb6b1032e761b6b5ba8fa7f447fa0af
Raíssa <raissajoaquim29@gmail.com>
2025-06-23 13:06:09 -0300

    botao de delete e tarefa ser exibida

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { useState } from 'react';
 import ProjectSidebar from "./Components/ProjectsSidebar";
 import NewProject from "./Components/NewProject";
 import NoProjectSelect from "./Components/NoProjectSelect";
+import SelectedProject from './Components/SelectedProject';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProject: undefined,
     projects: []
   });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id,
+      };
+    });
+  };
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -44,12 +54,24 @@ function App() {
     });
   }
 
-  console.log(projectsState);
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter(
+          (project) => project.id !== prevState.selectedProjectId
+        ),
+      };
+    });
+  };
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelect onStartAddProject={handleStartAddProject} />
   }
@@ -59,6 +81,7 @@ function App() {
       <ProjectSidebar
         onStartAddProject={handleStartAddProject}
         projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
       />
       {content}
     </main>
---
+++ src/Components/Tasks.jsx
@@ -0,0 +1,11 @@
+export default function Tasks() {
+    return (
+        <section>
+            <h2 className="">Tasks</h2>
+            <p>this project does not have any tasks yet</p>
+            <ul>
+
+            </ul>
+        </section>
+    );
+}
